d287723cc3433ed2d9e68e077c337745fe488c5f
Endléss <77249103+endlessjs01@users.noreply.github.com>
2021-02-17 23:18:59 +0300

    Update endless.js

--- endless.js
+++ endless.js
@@ -1,5 +1,5 @@
 module.exports = {
-  "token": "ODAxNzk5Mzk2Nzg2MTc2MDIw.YAl74A.Fb-XNHC34g4rDj1XI5uoyqu9wFY",
-  "geliştiriciler": ["584417088391741450"], 
-  "prefix": "!"
+  "token": "",
+  "geliştiriciler": [""], 
+  "prefix": ""
 };
